1f681148556b04ecab5d616f5ab4af79021707a3
unknown <artemshomin@gmail.com>
2020-12-10 17:01:07 +0300

    схема sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE doingsdone
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+USE doingsdone;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(128) NOT NULL,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    password CHAR(64) NOT NULL UNIQUE,
+    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dt_task TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    st_check INT(2) DEFAULT 0,
+    title_task VARCHAR(128) NOT NULL, 
+    dl_file NVARCHAR(max),
+    dt_end TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE projects (
+    title_project VARCHAR(128) NOT NULL
+);
