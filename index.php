28846d30674f273ae9f6a3f7fd82911e8ef805d2
unknown <artemshomin@gmail.com>
2020-12-16 17:02:38 +0300

    подключаем базу

--- index.php
+++ index.php
@@ -8,9 +8,14 @@ else {
     print("Соединение установлено");
 }
 
+$sql = "SELECT username, title_project FROM users, projects WHERE users.id = projects.id AND users.id = 1";
+$result = mysqli_query($dd_conf, $sql);
+$rows = mysqli_fetch_array($result, MYSQLI_ASSOC);
 require_once('templates/functions.php');
 require_once('templates/data.php');
 
+
+print($rows);
 $page_content = include_template('main.php', ['projects' => $projects, 'tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
 $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Дела в порядке']);
 print($layout_content);
