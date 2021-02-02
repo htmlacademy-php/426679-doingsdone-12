6bcb880f27e3a2cf103d1bb13b84e5b284de0a41
sirnet <artem@home-pc>
2021-02-02 21:50:54 +0300

    Правки

---
+++ add.php
@@ -0,0 +1,48 @@
+<?php
+
+$link = conect();
+$dt_end = 'null';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $requireds = ['name', 'project'];
+    $title_tasks = $_POST['name'];
+    $projects_id = $_POST['project'];
+    $sql = "SELECT projects.id, title_project
+            FROM projects
+            WHERE title_project = '". $_POST['project'] . "'";
+    $result = mysqli_query($link, $sql);
+    if($result){
+        $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        foreach($result as $res){
+            $projects_id = $res['id'];
+        }
+    }
+    if(is_date_valid($_POST['date'])){
+        $dt_end = $_POST['date'];
+    }
+    $errors = [];
+    foreach($requireds as $required){
+        if(empty($_POST[$required])){
+            $errors[$required] = 'error';
+        }
+    }
+    if (count($errors)) {
+        $page_content = include_template('addTask.php', ['errors' => $errors, 'projects' => $projects,'tasks' => $tasks]);
+    }
+    else {
+        if(isset($_FILES['file'])){
+            $file_name = $_FILES['file']['name'];
+            if($file_name){
+                $file_path = __DIR__ . '/uploads/';
+                move_uploaded_file($_FILES['file']['tmp_name'], $file_path . $file_name);
+            }else {
+                $file_name = null;
+            }
+        }
+        $sql = "INSERT INTO tasks (user_id, project_id, title_task, dt_end, dl_file) VALUES ( ". user_db() .", $projects_id, '$title_tasks', '$dt_end' , '$file_name' )";
+        $stmt = mysqli_prepare($link, $sql);
+        $res = mysqli_stmt_execute($stmt);
+        header("Location: index.php");
+    }
+}
+?>
