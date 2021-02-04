7e1b4b4bbac4b3baadcd52b33cdf40fda55a18f7
unknown <artemshomin@gmail.com>
2021-02-04 17:52:07 +0300

    Проверка страницы

--- register.php
+++ register.php
@@ -1,3 +1,14 @@
 <?php
     require_once('templates/functions.php');
+
+    $link = conect();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $errors = userSearch($link, $email);
+    }
+
+    register();
 ?>
