3d3a42d826b09b21ce97dd29a04c534a0d3cec56
sirnet <artem@home-pc>
2021-02-08 21:20:50 +0300

    Создал шаблон авторизации

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ require_once('templates/functions.php');
 
 
 //Добавляем задачу
-
-$page_content = include_template('main.php', ['projects' => $projects, 'tasks' => $tasks, 'tasks_sort' => $tasks_sort, 'sort' => $sort, 'show_complete_tasks' => $show_complete_tasks]);
+if (isset($_SESSION['username'])) {
+    $page_content = include_template('main.php', ['projects' => $projects, 'tasks' => $tasks, 'tasks_sort' => $tasks_sort, 'sort' => $sort, 'show_complete_tasks' => $show_complete_tasks]);
+}
+else {
+    $page_content = include_template('guest.php', ['session' => $_SESSION['username']]);
+}
 
 
 layout($page_content);
