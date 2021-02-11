225eb91c50fd92d6f1b2a6576c159bc89ef75dca
sirnet <artem@home-pc>
2021-02-11 21:58:00 +0300

    Поиск

--- index.php
+++ index.php
@@ -12,6 +12,18 @@ else {
     $page_content = include_template('guest.php',['errors' => $errors]);
 }
 
+$search = $_GET['q'] ?? '';
+
+if($search){
+    $sql = "SELECT * FROM tasks " . "JOIN users ON tasks.user_id = users.id " .
+    "WHERE MATCH(title_task) AGAINST(?)";
+    $link = conect();
+    $stmt = db_get_prepare_stmt($link, $sql, [$search]);
+		mysqli_stmt_execute($stmt);
+	$result = mysqli_stmt_get_result($stmt);
+    $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
 
 layout($page_content,$userName);
 
